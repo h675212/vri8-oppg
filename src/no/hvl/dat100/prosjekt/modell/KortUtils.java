1b678ad823dab5e92a2efe44bcde07c8d22a467f
h675212 <675212@stud.hvl.no>
2023-10-16 14:57:21 +0200

    wowie

--- src/no/hvl/dat100/prosjekt/modell/KortUtils.java
+++ src/no/hvl/dat100/prosjekt/modell/KortUtils.java
@@ -6,6 +6,9 @@ import no.hvl.dat100.prosjekt.TODO;
 
 public class KortUtils {
 
+	public KortUtils() {
+		
+	}
 	/**
 	 * Sorterer en samling. Rekkefølgen er bestemt av compareTo() i Kort-klassen.
 	 * 
@@ -16,12 +19,26 @@ public class KortUtils {
 	 */
 	
 	public static void sorter(KortSamling samling) {
-		
 		// TODO - START
-		
+	        Kort[] kortTabell = samling.getSamling();
+	        int antallKort = samling.getAntalKort();
+
+	        // Bruk en enkel boblesortering for å sortere kortene
+	        for (int i = 0; i < antallKort - 1; i++) {
+	            for (int j = 0; j < antallKort - i - 1; j++) {
+	                if (kortTabell[j].compareTo(kortTabell[j + 1]) > 0) {
+	                    // Bytt plass på kortene
+	                    Kort temp = kortTabell[j];
+	                    kortTabell[j] = kortTabell[j + 1];
+	                    kortTabell[j + 1] = temp;
+	    // TODO - END                
+	                }
+	            }
+	        }
+	    }
+
 		
 		// TODO - END
-	}
 	
 	/**
 	 * Stokkar en kortsamling. 
@@ -33,8 +50,21 @@ public class KortUtils {
 		
 		// TODO - START
 		
-		throw new UnsupportedOperationException(TODO.method());
+	    Kort[] kortTabell = samling.getSamling();
+        int antallKort = samling.getAntalKort();
+        Random random = new Random();
+
+        // Bruk Fisher-Yates algoritme for å stokke kortene
+        for (int i = antallKort - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            // Bytt plass på kortene
+            Kort temp = kortTabell[i];
+            kortTabell[i] = kortTabell[j];
+            kortTabell[j] = temp;
+        }
+    }
+
 		// TODO - END
-	}
-	
 }
+	
+
